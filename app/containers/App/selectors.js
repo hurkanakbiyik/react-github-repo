60ba1c76d13d91de37eeb2bdac41c7e99d277eaf
hurkanakbiyik <hurkanakbiyik@gmail.com>
2018-06-20 10:52:04 +0300

    Add selectors for loading and error.

--- app/containers/App/selectors.js
+++ app/containers/App/selectors.js
@@ -8,12 +8,25 @@ const selectGlobal = (state) => state.get('global');
 
 const selectRoute = (state) => state.get('route');
 
+const makeSelectLoading = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.get('loading')
+);
+
+const makeSelectError = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.get('error')
+);
+
 const makeSelectLocation = () => createSelector(
   selectRoute,
   (routeState) => routeState.get('location').toJS()
 );
 
+
 export {
   selectGlobal,
   makeSelectLocation,
+  makeSelectError,
+  makeSelectLoading
 };
